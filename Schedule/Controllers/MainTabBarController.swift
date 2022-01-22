ccb06fb1e1be319652a5d2028eeda865ea55d92b
Sergei Semko <semko431@gmail.com>
2022-01-22 18:52:05 +0300

    TabBarController/NavigationController, FSCalendar

--- Schedule/Controllers/MainTabBarController.swift
+++ Schedule/Controllers/MainTabBarController.swift
@@ -7,14 +7,35 @@
 
 import UIKit
 
-class MainTabBarController: UIViewController {
+class MainTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .brown
+        setupTabBar()
+        
+    }
+    
+    func setupTabBar() {
+        let sheduleViewController = createNavController(vc: SheduleViewController(), itemName: "Shedule", ItemImage: "calendar.badge.clock")
+        let tasksViewController = createNavController(vc: TasksViewController(), itemName: "Tasks", ItemImage: "text.badge.checkmark")
+        let contactsViewController = createNavController(vc: ContactsViewController(), itemName: "Contacts", ItemImage: "rectangle.stack.person.crop")
+        
+        viewControllers = [sheduleViewController, tasksViewController, contactsViewController]
     }
 
-
+    func createNavController(vc: UIViewController, itemName: String, ItemImage: String) -> UINavigationController {
+        
+        let item = UITabBarItem(title: itemName, image: UIImage(systemName: ItemImage), tag: 0)
+        
+//        let item = UITabBarItem(title: itemName, image: UIImage(systemName: ItemImage)?.withAlignmentRectInsets(.init(top: 10, left: 0, bottom: 0, right: 0)), tag: 0)
+        
+//        item.titlePositionAdjustment = .init(horizontal: 0, vertical: 10)
+        
+        let navController = UINavigationController(rootViewController: vc)
+        navController.tabBarItem = item
+        
+        return navController
+    }
 }
 
---
+++ Schedule/Controllers/SheduleViewController.swift
@@ -0,0 +1,134 @@
+//
+//  SheduleViewController.swift
+//  Schedule
+//
+//  Created by justSmK on 22.01.2022.
+//
+
+import UIKit
+import FSCalendar
+
+class SheduleViewController: UIViewController {
+
+    var calendarHeightConstraint: NSLayoutConstraint!
+    
+    private var calendar: FSCalendar = {
+        let calendar = FSCalendar()
+        calendar.translatesAutoresizingMaskIntoConstraints = false
+        return calendar
+    }()
+    
+    let showHideButton: UIButton = {
+        let button = UIButton()
+        
+        button.setTitle("Open calendar", for: .normal)
+        button.setTitleColor(.darkGray, for: .normal)
+        button.titleLabel?.font = UIFont(name: "Avenir Next Demi Bold", size: 14)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+        
+    }()
+    
+    let tableView: UITableView = {
+       let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    let isSheduleCell = "idSheduleCell"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        title = "Shedule"
+        
+        calendar.delegate = self
+        calendar.dataSource = self
+        calendar.scope = .week
+        
+        setConstraints()
+        swipeAction()
+        
+        showHideButton.addTarget(self, action: #selector(showHideButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc func showHideButtonTapped() {
+        if calendar.scope == .week {
+            calendar.setScope(.month, animated: true)
+            showHideButton.setTitle("Close calendar", for: .normal)
+        } else {
+            calendar.setScope(.week, animated: true)
+            showHideButton.setTitle("Open calendar", for: .normal)
+        }
+    }
+    
+    
+    //MARK: SwipeGestureRecognizer
+    
+    func swipeAction() {
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe))
+        swipeUp.direction = .up
+        calendar.addGestureRecognizer(swipeUp)
+        
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe))
+        swipeDown.direction = .down
+        calendar.addGestureRecognizer(swipeDown)
+    }
+    
+    @objc func handleSwipe(gesture: UISwipeGestureRecognizer) {
+        switch gesture.direction {
+        case .up: showHideButtonTapped()
+        case .down: showHideButtonTapped()
+        default:
+            break
+        }
+    }
+    
+    
+    
+}
+
+//MARK: FSCalendarDataSource, FSCalendarDelegate
+
+extension SheduleViewController: FSCalendarDataSource, FSCalendarDelegate {
+    
+    func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
+        calendarHeightConstraint.constant = bounds.height
+        view.layoutIfNeeded()
+    }
+    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        print(date)
+    }
+    
+}
+
+
+//MARK: SetConstraints
+
+extension SheduleViewController {
+    func setConstraints() {
+        
+        view.addSubview(calendar)
+        
+        calendarHeightConstraint = NSLayoutConstraint(item: calendar, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 300)
+        calendar.addConstraint(calendarHeightConstraint)
+        
+        NSLayoutConstraint.activate([
+            calendar.topAnchor.constraint(equalTo: view.topAnchor, constant: 90),
+            calendar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            calendar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+        ])
+        
+        view.addSubview(showHideButton)
+        
+        NSLayoutConstraint.activate([
+            showHideButton.topAnchor.constraint(equalTo: calendar.bottomAnchor, constant: 0),
+            showHideButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+            showHideButton.widthAnchor.constraint(equalToConstant: 100),
+            showHideButton.heightAnchor.constraint(equalToConstant: 20)
+        ])
+    }
+}
+
