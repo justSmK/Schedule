db240775b0c357942ea5f34e9633aa2d869fa772
Sergei Semko <semko431@gmail.com>
2022-01-22 18:44:44 +0300

    Setup project

--- Schedule/Controllers/MainTabBarController.swift
+++ Schedule/Controllers/MainTabBarController.swift
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainTabBarController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .brown
     }
 
 
